a62e3a3353cc4eb75a2afe671392cd50409fdcc6
Alan Cooper <ascooper57@users.noreply.github.com>
2025-02-27 10:10:46 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version:5.0
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 //  Package.swift
 //
 //  Created by Dave Carlson on 8/8/19.
@@ -8,16 +9,17 @@ import PackageDescription
 let package = Package(
     name: "SMART",
 	platforms: [
-		.macOS(.v10_13), .iOS(.v11)
-	],
+        .macOS(.v10_15),
+        .iOS(.v12),
+    ],
     products: [
         .library(
             name: "SMART",
             targets: ["SMART"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/breatheai/Swift-FHIR", "4.2.0"..<"5.0.0"),
-		.package(url: "https://github.com/p2/OAuth2", "5.1.0"..<"6.0.0"),
+        .package(url: "https://github.com/breatheai/Swift-FHIR.git", from: "4.3.0"),
+        .package(url: "https://github.com/p2/OAuth2.git", from: "5.1.0"),
     ],
     targets: [
 		.target(
@@ -28,5 +30,23 @@ let package = Package(
 			],
 			path: "Sources",
 			sources: ["SMART", "Client", "iOS", "macOS"]),
-    ]
+        .testTarget(
+            name: "ClientTests",
+            dependencies: ["SMART"]),
+        .testTarget(
+            name: "ServerTests",
+            dependencies: ["SMART"]),
+    ],
+    swiftLanguageModes: [.version("5.10"), .version("6")]
 )
+
+
+func testDependencies() -> [PackageDescription.Target.Dependency] {
+    #if os(macOS) || os(iOS)
+        [
+            "SMART",
+            .product(name: "FHIR", package: "Swift-FHIR"),
+            .product(name: "OAuth2", package: "OAuth2"),
+        ]
+    #endif
+}
